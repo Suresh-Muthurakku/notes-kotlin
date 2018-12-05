14d9ca097eb3954100f2829a2e9ecbd33767c9eb
Suresh-Muthurakku <suresh.muthurakku@gmail.com>
2018-12-05 15:34:51 +0100

    notes first draft

---
+++ src/main/kotlin/com/self/kotlin/notes/NotesApplication.kt
@@ -0,0 +1,11 @@
+package com.self.kotlin.notes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotesApplication
+
+fun main(args: Array<String>) {
+    runApplication<NotesApplication>(*args)
+}
